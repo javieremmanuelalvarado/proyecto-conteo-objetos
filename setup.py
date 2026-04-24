5cd0157c670b17d93c4bbaaf6acb45d35b97445b
javieremmanuelalvarado <javieremmanuelalvarado3795@gmail.com>
2026-04-24 16:50:29 -0600

    Actualiza proyecto con README, setup, corner detection y mejoras

--- setup.py
+++ setup.py
@@ -5,3 +5,17 @@ Created on Tue Apr 21 22:36:54 2026
 @author: emman
 """
 
+from setuptools import setup, find_packages
+
+setup(
+    name="contador_objetos",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "matplotlib",
+        "opencv-python"
+    ],
+    author="Javier Emmanuel Alvarado",
+    description="Librería para segmentación y conteo de objetos en imágenes",
+)
