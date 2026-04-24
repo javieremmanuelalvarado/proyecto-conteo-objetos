0a56d4862136ef621fc4172d8825a48890549452
javieremmanuelalvarado <javieremmanuelalvarado3795@gmail.com>
2026-04-24 16:57:08 -0600

    Elimina archivos caracteristicas.py y pipeline.py no utilizados

--- contador_objetos/caracteristicas.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 21 22:29:39 2026
-
-@author: emman
-"""
-
--- contador_objetos/pipeline.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Tue Apr 21 22:30:09 2026
-
-@author: emman
-"""
-
